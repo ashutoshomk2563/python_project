6a5bce39d2dd8ad2c268db8cb8bf31646f698949
ashutoshomk2563 <ashutoshomk2563@gmail.com>
2025-04-23 15:48:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print("hello world")
+
+with open("example.txt", "w") as file:
+    file.write("Hello, this file was created using Python!\n")
+    file.write("You can write as many lines as you want.\n")
+
+print("File created successfully!")
